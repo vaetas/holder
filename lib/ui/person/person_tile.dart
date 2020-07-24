918ee800a458385b2bb90a5254a1e1dde6fe2a80
Vojtech Pavlovsky <vojtech.pavlovsky@gmail.com>
2020-07-24 22:18:18 +0200

    Add avatar

--- lib/ui/person/person_tile.dart
+++ lib/ui/person/person_tile.dart
@@ -5,18 +5,33 @@ import 'package:holder/ui/person/person_screen.dart';
 class PersonTile extends StatelessWidget {
   final Person person;
 
-  PersonTile({this.person});
+  PersonTile({Key key, this.person}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(person.fullName),
       subtitle: Text(person.description),
+      leading: _Avatar(),
       onTap: () {
         Navigator.of(context).push(MaterialPageRoute(
           builder: (context) => PersonScreen(id: person.id),
         ));
       },
+    );
+  }
+}
+
+class _Avatar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Theme.of(context).primaryColor.withOpacity(0.075),
+      ),
+      child: Icon(Icons.person),
     );
   }
 }
