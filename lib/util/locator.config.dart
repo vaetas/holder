2f602a6c7397bb50266d9b8efa48aa6c89b6a6db
Vojtech Pavlovsky <vojtech.pavlovsky@gmail.com>
2020-08-11 22:56:48 +0200

    Update dependencies

--- lib/util/locator.config.dart
+++ lib/util/locator.config.dart
@@ -4,22 +4,24 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
-import 'dart:async';
-
 import 'package:get_it/get_it.dart';
-import 'package:injectable/get_it_helper.dart';
+import 'package:injectable/injectable.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import '../service/authentication_service.dart';
-import '../service/module.dart';
-import '../service/preferences_service.dart';
 import 'database.dart';
+import '../service/authentication_service.dart';
+import '../service/preferences_service.dart';
+import '../service/module.dart';
 
 /// adds generated dependencies
 /// to the provided [GetIt] instance
 
-Future<void> $initGetIt(GetIt g, {String environment}) async {
-  final gh = GetItHelper(g, environment);
+Future<GetIt> $initGetIt(
+  GetIt get, {
+  String environment,
+  EnvironmentFilter environmentFilter,
+}) async {
+  final gh = GetItHelper(get, environment, environmentFilter);
   final registerModule = _$RegisterModule();
   gh.lazySingleton<AuthenticationService>(() => AuthenticationService());
   gh.lazySingleton<PreferencesService>(() => PreferencesService());
@@ -29,6 +31,7 @@ Future<void> $initGetIt(GetIt g, {String environment}) async {
   // Eager singletons must be registered in the right order
   final appDatabase = await registerModule.database;
   gh.singleton<AppDatabase>(appDatabase);
+  return get;
 }
 
 class _$RegisterModule extends RegisterModule {}
